b0a41f5f105ff08dae8a229c36eb2440fb4ce2b0
Valentin Chauveau <valentin.korea@gmail.com>
2022-09-06 16:34:59 +0200

    font fix in prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,6 +78,8 @@ Rails.application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
+  config.assets.precompile << /\.(?:svg|eot|woff|woff2|ttf)\z/
+
   # Use a different logger for distributed setups.
   # require "syslog/logger"
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new "app-name")
