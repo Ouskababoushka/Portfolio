ed9d5fc110bd1bf2b974fceb26f2e551acae7add
Valentin Chauveau <valentin.korea@gmail.com>
2022-09-13 14:49:00 +0200

    gsap not working

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -7,3 +7,9 @@ application.debug = false
 window.Stimulus   = application
 
 export { application }
+
+//= require 'greensock/TweenLite'
+//= require 'greensock/easing/EasePack'
+//= require 'greensock/TimeLineLite'
+//= require 'greensock/plugins/CSSPlugin'
+//= require 'greensock/plugins/BezierPlugin'
